a2dc1c7727d575e35c857033eb1991db0bfefd06
LeoTchies <leo.tolotti.chies@gmail.com>
2025-08-12 16:34:37 -0300

    Update game.js
    Ajuste da configurações de Javascript para desenvolvimento do jogo

--- game.js
+++ game.js
@@ -1,5 +1,23 @@
 var coresBotoes = ["red", "blue", "green", "yellow"];
 var padraoDeJogo = [];
+var padraoBotoesClicados = [];
+var inicio = true;
+var level = 0;
+
+$("html").keydown(function(){
+  $("#level-title").text("Level " + level)
+  proximaSequencia();
+  inicio = false;
+  })
+
+
+$(".btn").click(function(){
+  var corEscolhidaPeloJogador = $(this).attr("id");
+  padraoBotoesClicados.push(corEscolhidaPeloJogador);
+  tocarMusica(corEscolhidaPeloJogador)
+  animarBotao(corEscolhidaPeloJogador)
+  conferirResposta(padraoBotoesClicados.length-1)
+})
 
 
 function proximaSequencia (){
@@ -9,11 +27,34 @@ function proximaSequencia (){
   padraoDeJogo.push(corRondomizada);
 
   $("#"+corRondomizada).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  var somVerde = new Audio ("sounds/"+ corRondomizada + ".mp3");
-  somVerde.play();
-
+  tocarMusica(corRondomizada);
+  
+  level++;
+  $("#level-title").text("Level " + level);
+  padraoBotoesClicados = [];
 }
 
+function tocarMusica (name){
+  var som = new Audio ("sounds/"+ name + ".mp3");
+  som.play();
+}
 
+function animarBotao (corAtual){
+  $("#" + corAtual).addClass("pressed");
+  setTimeout(function(){
+    $("#" + corAtual).removeClass("pressed");}, 300);
 
+  }
 
+function conferirResposta (levelAtual){
+  if (padraoDeJogo[levelAtual] === padraoBotoesClicados[levelAtual]){
+    console.log("Sucesso");
+    if(padraoDeJogo.length === padraoBotoesClicados.length){
+        setTimeout(function(){
+          proximaSequencia();
+        }, 2000);
+      }
+    }
+
+   else {console.log("Erroooooooou")}
+  }
